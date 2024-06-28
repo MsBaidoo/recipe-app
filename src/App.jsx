29c94c016b49decfff2d71a2f626065e44c57dac
mseve <evelynyaabaidoo@gmail.com>
2024-06-28 09:29:15 +0000

    React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
+import Recipes from "./pages/Recipes"
 import Welcome from "./pages/welcome"
+import Categories from "./pages/categories"
+
+const router = createBrowserRouter([
+  {path: "/", element: <Welcome/>},
+  {path: '/recipes', element: <Recipes/>},
+  {path: '/categories', element: <Categories/>}
+])
 
 function App() {
   
 
   return (
     <>
-      <Welcome/> 
+      <RouterProvider router={router}/>
     </>
   )
 }
--- src/pages/welcome.jsx
+++ src/pages/welcome.jsx
@@ -1,12 +1,28 @@
-import welcomeImage from "../assets/images/welcome-image.png";r-58
+import welcomeImage from "../assets/images/welcome-image.png";
 export default function Welcome() {
 
-    return(
-        <div>
+    return (
+        <div className="bg-[linear-gradient(rgba(0,0,0,0.3),rgba(0,0,0,0.5)),url('./assets/images/welcome-bg.png')] bg-cover h-screen flex flex-col place-items-center place-content-center">
 
-    <h4>Savory & Sweet</h4>
-    <img src={welcomeImage} alt="welcome Image" />
+            <h4 className="text-white text-center text-4xl">Savory & <br /> Sweet</h4>
+            <img src={welcomeImage} alt="welcome Image" />
 
-    </div>   
+        </div>
     )
-}
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
